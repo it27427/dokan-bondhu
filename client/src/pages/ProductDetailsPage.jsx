a154510c5f41d42d2c65ff491dbc8c2c36b346a9
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:20:55 +0600

    RootLayout added in ProductDetails Page

--- client/src/pages/ProductDetailsPage.jsx
+++ client/src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout.jsx';
 
 const ProductDetailsPage = () => {
   return (
-    <>
+    <RootLayout>
       <h2 className='text-2xl'>Product Details Page</h2>
-    </>
+    </RootLayout>
   );
 };
 
