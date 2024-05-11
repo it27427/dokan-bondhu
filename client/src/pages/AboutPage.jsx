daa1a893d7b8f0ebe194def44a7ae41ba8b76a76
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:19:08 +0600

    RootLayout added in about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout.jsx';
 
 const AboutPage = () => {
   return (
-    <>
+    <RootLayout>
       <h2 className='text-2xl'>About Page</h2>
-    </>
+    </RootLayout>
   );
 };
 
