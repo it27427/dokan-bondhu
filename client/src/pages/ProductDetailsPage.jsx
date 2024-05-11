5a45634a9ad0cb5a8b26301aabd761e383f1c135
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:21:05 +0600

    product-details content

---
+++ client/src/pages/ProductDetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetailsPage = () => {
+  return (
+    <>
+      <h2 className='text-2xl'>Product Details Page</h2>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
