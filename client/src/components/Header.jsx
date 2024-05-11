60344090c68e0445466cee07ac2329b47d57eeb1
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:25:46 +0600

    headercomponent content

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 z-50'>
+      <div className='container'>
+        <h2 className='text-2xl'>Header Component</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
