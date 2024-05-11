0069c2fb18b06b29d6f3bcd47cb99581c4f7ad6a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:23:57 +0600

    DefaultLayout added in Register page

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import DefaultLayout from '@/layouts/DefaultLayout';
 
 const RegisterPage = () => {
   return (
-    <>
+    <DefaultLayout>
       <h2 className='text-3xl'>Register Page</h2>
-    </>
+    </DefaultLayout>
   );
 };
 
