d6c7a3983151f849231432ac0ee31caf776262e0
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 12:54:07 +0600

    container-padding added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     },
     container: {
       center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
   },
   plugins: [],
