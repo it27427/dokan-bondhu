59d211bb95bd155270230a6f422669a9fd978a99
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:27:52 +0600

    RootLayout-Updated

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const RootLayout = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default RootLayout;
