35a4ca91ee0e720e11adb1db57e1053b1236feb6
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 13:49:33 +0600

    server-listening both frontend-and-backend

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const app = require('./app');
 const colors = require('colors');
+const config = require('./config/config');
+
+const PORT = config.app.port;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is successfully listening at http://localhost:${PORT}`.bgCyan
+  );
+});
