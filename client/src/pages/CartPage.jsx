11e4fcf5ffde39bfdec9e87c8739c550a12f045e
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:19:46 +0600

    RootLayout added in CartPage

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout.jsx';
 
 const CartPage = () => {
   return (
-    <>
+    <RootLayout>
       <h2 className='text-2xl'>Cart Page</h2>
-    </>
+    </RootLayout>
   );
 };
 
