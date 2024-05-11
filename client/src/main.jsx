8441187809b685e7c35640588a88420b388a5d5f
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 12:58:17 +0600

    remix-icon added in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
+// REMIX-ICON
+import 'remixicon/fonts/remixicon.css';
+// TAILWIND-CSS
 import '@/assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
