5595795360fac3021dc2629c5a4371ad04cb96e3
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:22:43 +0600

    DefaultLayout added in notfound page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import DefaultLayout from '@/layouts/DefaultLayout.jsx';
 
 const NotFoundPage = () => {
   return (
-    <>
+    <DefaultLayout>
       <h2 className='text-2xl'>Not Found Page</h2>
-    </>
+    </DefaultLayout>
   );
 };
 
