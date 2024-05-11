634838626f3b1130364bf8bf0a85f05c7061558a
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 16:21:58 +0600

    RootLayout added in CheckoutPage

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import RootLayout from '@/layouts/RootLayout.jsx';
 
 const CheckoutPage = () => {
   return (
-    <>
+    <RootLayout>
       <h2 className='text-3xl'>Checkout Page</h2>
-    </>
+    </RootLayout>
   );
 };
 
