27bb4ac0ff6b7992b0c08c763daeb62a428fa2c6
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:03:30 +0600

    vite-config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       pages: path.resolve(__dirname, './src/pages'),
       features: `${path.resolve(__dirname, './src/features')}`,
       assets: `${path.resolve(__dirname, './src/assets')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks')}`,
     },
   },
   server: {
