f38c399bf883ec7c3c475475a900c87aa0fc3fca
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:40:19 +0600

    App Updated by Page Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from '@/pages/HomePage';
 import AboutPage from '@/pages/AboutPage';
+import ContactPage from '@/pages/ContactPage';
 import ShopPage from '@/pages/ShopPage';
+import ProductDetailsPage from '@/pages/ProductDetailsPage';
 import CartPage from '@/pages/CartPage';
 import CheckoutPage from '@/pages/CheckoutPage';
+import NotFoundPage from '@/pages/NotFoundPage';
+
+// AUTH-PAGES
+import LoginPage from '@/pages/auth/LoginPage';
+import RegisterPage from '@/pages/auth/RegisterPage';
+import ForgotPage from '@/pages/auth/ForgotPage';
 
 const App = () => {
   return (
@@ -12,9 +20,19 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/product-details' element={<ProductDetailsPage />} />
         <Route path='/cart' element={<CartPage />} />
         <Route path='/checkout' element={<CheckoutPage />} />
+
+        {/* AUTH-PAGES */}
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/forgot-password' element={<ForgotPage />} />
+
+        {/* NOT-FOUND-PAGE */}
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
