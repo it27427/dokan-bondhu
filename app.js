829aa0094a73644217be71f580e77cea915efb34
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 13:11:07 +0600

    app done

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from home route</h1>');
+});
+
+module.exports = app;
