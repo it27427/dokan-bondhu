48fb78f3a816cab50b233b2e13f6d6ebf6345779
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-11 14:24:27 +0600

    not-found page content

---
+++ client/src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <h2 className='text-2xl'>Not Found Page</h2>
+    </>
+  );
+};
+
+export default NotFoundPage;
