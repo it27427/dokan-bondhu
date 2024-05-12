75241b5287cd567da7db8db87b4db706b4044a24
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-12 11:24:10 +0600

    usermodel

--- models/user.model.js
+++ models/user.model.js
@@ -1,43 +1,55 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const { v4 as uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxlength: 50
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    history: {
+      type: Array,
+      default: [],
+    },
   },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true
-  },
-  phone: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-    trim: true
-  },
-  salt: String,
-  role: {
-    type: Number,
-    default: 0
-  },
-  history: {
-    type: Array,
-    default: []
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
+
+userSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.password = bcrypt.genSalt();
+  })
+  .get();
 
 const User = mongoose.model('User', userSchema);
 
