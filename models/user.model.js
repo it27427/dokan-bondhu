ac67210d21b6acca6cf00e4a9cfe723784020da4
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-12 11:04:01 +0600

    userSchema

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,43 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const { v4 as uuidv4 } = require('uuid');
 
-const userSchema = mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    trim: true
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0
+  },
+  history: {
+    type: Array,
+    default: []
+  }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
